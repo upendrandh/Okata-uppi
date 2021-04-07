34872fe67efe49dffdfe5340652a36e2c8227dd5
hbnreddy <73657652+hbnreddy@users.noreply.github.com>
2021-04-07 12:52:33 +0530

    Update GreetingControllerTest.java

--- src/test/java/com/okta/developer/simpleapp/GreetingControllerTest.java
+++ src/test/java/com/okta/developer/simpleapp/GreetingControllerTest.java
@@ -1,42 +1,24 @@
 package com.okta.developer.simpleapp;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
 import org.springframework.security.core.GrantedAuthority;
-
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-
 import org.springframework.test.context.ContextConfiguration;
-
 import org.springframework.test.web.servlet.MockMvc;
-
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import java.time.Instant;
 import java.util.ArrayList;
-
 import java.util.Collection;
-
 import java.util.HashMap;
-
 import java.util.Map;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @AutoConfigureMockMvc
@@ -74,40 +56,25 @@ public class GreetingControllerTest {
 
                 .andExpect(MockMvcResultMatchers.status().isOk())
 
-                .andExpect(content().string("Hello user@email.com"));
+                .andExpect(content().string("harindra48@gmail.com"));
 
     }
 
 
     private OAuth2AuthenticationToken createMockOAuth2AuthenticationToken(OidcIdToken idToken) {
-
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-
         OidcUser user = new DefaultOidcUser(authorities, idToken);
-
-
         return new OAuth2AuthenticationToken(user, authorities, "oidc");
-
     }
 
-
-
     private OidcIdToken createOidcToken(){
-
         Map<String, Object> claims = new HashMap<>();
-
         claims.put("groups", "ROLE_USER");
-
-        claims.put("email", "user@email.com");
-
+        claims.put("email", "harindra48@gmail.com");
         claims.put("sub", 123);
-
         OidcIdToken idToken = new OidcIdToken(ID_TOKEN, Instant.now(),
-
                 Instant.now().plusSeconds(60), claims);
-
         return idToken;
 
     }
